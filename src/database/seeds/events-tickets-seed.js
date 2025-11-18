6f864d572eec8531e1a69dc73b6c7e3faeffe6df
dvill <villalbautria08@gmail.com>
2025-11-18 18:55:41 -0500

    improve events seed execution time

--- src/database/seeds/events-tickets-seed.js
+++ src/database/seeds/events-tickets-seed.js
@@ -1,25 +1,23 @@
 const { faker } = require('@faker-js/faker')
 
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // create events and tickets for those events
-  for (let i = 0; i < 100; i++) {
-    await knex('events').insert({
+  // added pre generated rows and batch insert for performance improvement
+  const eventsData = new Array(100).fill(null)
+    .map(() => ({
       name: faker.lorem.words(),
       description: faker.lorem.paragraph(),
       date: faker.date.future(),
       location: faker.location.city(),
-    })
-  }
+    }))
+  await knex('events').insert(eventsData)
   const events = await knex('events').select('id')
-  for (const event of events) {
-    // create a 500 allotment of tickets for this event
-    for (let j = 0; j < 500; j++) {
-      await knex('tickets').insert({
-        event_id: event.id,
-        status: faker.helpers.arrayElement(['available', 'sold', 'reserved']),
-        type: 'general',
-        price: 1000,
-      });
-    }
-  }
+  const tickets = events.reduce((tickets, event) => tickets.concat(new Array(500).fill(null).map(() => ({
+    event_id: event.id,
+    status: faker.helpers.arrayElement(['available', 'sold', 'reserved']),
+    type: 'general',
+    price: faker.number.int(1000),
+  }))), [])
+
+  await knex.batchInsert('tickets', tickets, 200)
 }
