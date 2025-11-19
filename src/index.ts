6859e480d348c1facf4a1fd46cb154ef4c4647f5
dvill <villalbautria08@gmail.com>
2025-11-18 19:40:10 -0500

    setup mongo instance

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
 import express from 'express';
 import { knex } from 'knex';
-import dbConfig  from './knexfile';
+import dbConfig from './knexfile';
 import { createEventDAL } from './dal/events.dal';
 import { createTicketDAL } from './dal/tickets.dal';
 import { createGetEventsController } from './controllers/get-events';
+import { MongoClient } from 'mongodb';
 
-// initialize Knex
+// initialize dependencies
 const Knex = knex(dbConfig.development);
+const mongoClient = new MongoClient('mongodb://root:example@localhost:27017');
+const mongoDb = mongoClient.db('main');
 
 // Initialize DALs
 const eventDAL = createEventDAL(Knex);
 const TicketDAL = createTicketDAL(Knex);
 
-
 const app = express();
+app.use(express.json());
 
-app.use('/health', (req, res) => {
+app.use('/health', (_req, res) => {
   res.json({ status: 'ok' });
 });
 
@@ -25,6 +28,8 @@ app.use('/', (_req, res) => {
   res.json({ message: 'Hello API' });
 });
 
-app.listen(3000, () => {
-  console.log('Server Started')
-});
+export default app;
+export const teardown = async () => {
+  await mongoClient.close();
+  await Knex.destroy();
+};
